de5dbf9e1d0fd937b01f4941f2096499811ff29b
tjdgmlrnsk <tjdgmlrnsk@e3c12c2a-3f9b-4cee-bc1f-3a9786f174a7>
2016-08-12 16:16:55 +0000

    - obj 폴더 삭제
    git-svn-id: svn://www.hshgames.com/project_s@126 e3c12c2a-3f9b-4cee-bc1f-3a9786f174a7

--- proj.android/obj/local/armeabi/objs-debug/cocos2dcpp_shared/__/__/Classes/MyUI/BulletCountUI.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dcpp_shared/__/__/Classes/MyUI/BulletCountUI.o: \
- jni/../../Classes/MyUI/BulletCountUI.cpp \
- jni/../../Classes/MyUI/BulletCountUI.h
-
-jni/../../Classes/MyUI/BulletCountUI.h:
--- proj.android/obj/local/armeabi/objs-debug/cocos2dcpp_shared/__/__/Classes/MyUI/CoinUI.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dcpp_shared/__/__/Classes/MyUI/CoinUI.o: \
- jni/../../Classes/MyUI/CoinUI.cpp jni/../../Classes/MyUI/CoinUI.h
-
-jni/../../Classes/MyUI/CoinUI.h:
