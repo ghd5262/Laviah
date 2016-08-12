d8af1830895e00648c42c88594215655159fa6f1
tjdgmlrnsk <tjdgmlrnsk@e3c12c2a-3f9b-4cee-bc1f-3a9786f174a7>
2016-08-12 17:33:53 +0000

    - UserDataManager 수정
    git-svn-id: svn://www.hshgames.com/project_s@133 e3c12c2a-3f9b-4cee-bc1f-3a9786f174a7

--- proj.win32/Classes/DataManager/UserDataManager.h
+++ proj.win32/Classes/DataManager/UserDataManager.h
@@ -9,29 +9,7 @@ struct sUSER_DATA{
     std::map<std::string, unsigned> _userDataUnsignedMap;
     std::map<std::string, std::vector<unsigned>*> _userDataListMap;
     std::map<std::string, std::string> _userDataKeyMap;
-    
-//	unsigned _saveRevision;
-//    unsigned _level;
-//    unsigned _coin;
-//    unsigned _bestTotalScore;
-//    unsigned _bestCombo;
-//    unsigned _currentCharacter;
-//    unsigned _currentPet;
-//    unsigned _currentRocket;
-//    unsigned _currentChallenge;
-//	unsigned _healthLevel;
-//	unsigned _starItemLimitTimeLevel;
-//	unsigned _coinItemLimitTimeLevel;
-//	unsigned _bonusItemLimitTimeLevel;
-//	unsigned _magnetItemLimitTimeLevel;
-//	unsigned _magnetItemLimitRadiusLevel;
-//	unsigned _giantItemLimitTimeLevel;
-//    std::vector<bool> _characterList;
-//    std::vector<bool> _petList;
-//    std::vector<bool> _rocketList;
-//    std::vector<bool> _completeChallengeList;
-//    std::vector<bool> _mtrlList;
-    
+
 };
 
 class CGoogleCloudManager;
@@ -55,66 +33,8 @@ public:
     void setUserData_ItemGet(std::string key, unsigned itemIdx);
     
     //getter & setter
-//	unsigned getSaveRevision();
 	void setSaveRevision(unsigned value);
     std::map<std::string, std::string> getKeyList() { return m_UserData->_userDataKeyMap; }
-//    unsigned getLevel();
-//    void setLevel(unsigned value);
-    
-//    unsigned getCoin();
-//    void setCoin(unsigned value);
-    
-//    unsigned getHealthLevel();
-//    void setHealthLevel(unsigned value);
-    
-//	unsigned getStarItemLimitTimeLevel();
-//	void setStarItemLimitTimeLevel(unsigned value);
-    
-//	unsigned getCoinItemLimitTimeLevel();
-//    void setCoinItemLimitTimeLevel(unsigned value);
-    
-//	unsigned getBonusItemLimitTimeLevel();
-//	void setBonusItemLimitTimeLevel(unsigned value);
-    
-//	unsigned getGiantItemLimitTimeLevel();
-//	void setGiantItemLimitTimeLevel(unsigned value);
-
-//	unsigned getMagnetItemLimitTimeLevel();
-//	void setMagnetItemLimitTimeLevel(unsigned value);
-    
-//	unsigned getMagnetItemLimitRadiusLevel();
-//	void setMagnetItemLimitRadiusLevel(unsigned value);
-
-//	unsigned getBestTotalScore();
-//	void setBestTotalScore(unsigned value);
-
-//	unsigned getBestCombo();
-//	void setBestCombo(unsigned value);
-
-//	unsigned getCurrentCharacter();
-//	void setCurrentCharacter(unsigned value);
-
-//	unsigned getCurrentPet();
-//	void setCurrentPet(unsigned value);
-
-//	unsigned getCurrentRocket();
-//	void setCurrentRocket(unsigned value);
-
-//	unsigned getCurrentChallenge();
-//	void setCurrentChallenge(unsigned value);
-
-    
-//    std::vector<std::string> getKeyList() { return m_UserData->_userDataKeyList; }
-    
-//	std::vector<bool> getUserDataHaveCharacterList() { return m_UserData->_characterList; }
-    
-    
-//	bool isCharacterHave(int characterIdx);
-//	void haveCharacter(int characterIdx);
-//    bool isPetHave(int petIdx);
-//    void havePet(int petIdx);
-//    bool isRocketHave(int rocketIdx);
-//    void haveRocket(int rocketIdx);
 
 	float getStarItemLimitTime();
 	float getCoinItemLimitTime();
@@ -134,49 +54,6 @@ private:
     void userDataLoad(std::string key, std::string valueJson);
     void userDataLoad_Number(std::string key, std::string valueJson);
     void userDataLoad_List(std::string key, std::string valueJson);
-    
-    
-    
-//	void dataSave_UserLevel();
-//	void dataSave_UserCoin();
-//	void dataSave_UserHealth();
-//	void dataSave_UserStarItemLimitTime();
-//	void dataSave_UserCoinItemLimitTime();
-//	void dataSave_UserGiantItemLimitTime();
-//	void dataSave_UserMagnetItemLimitTime();
-//	void dataSave_UserMagnetItemLimitRadius();
-//	void dataSave_UserBonusItemLimitTime();
-//	void dataSave_UserBestTotalScore();
-//	void dataSave_UserBestCombo();
-//	void dataSave_UserCurrentCharacter();
-//	void dataSave_UserCurrentPet();
-//	void dataSave_UserCurrentRocket();
-//	void dataSave_UserCurrentChallenge();
-//	void dataSave_UserCharacterList();
-//	void dataSave_UserPetList();
-//	void dataSave_UserRocketList();
-    
-
-//    void dataLoad_UserDataSaveRevision(std::string value);
-//    void dataLoad_UserLevel(std::string value);
-//    void dataLoad_UserCoin(std::string value);
-//    void dataLoad_UserHealth(std::string value);
-//    void dataLoad_UserStarItemLimitTime(std::string value);
-//    void dataLoad_UserCoinItemLimitTime(std::string value);
-//    void dataLoad_UserGiantItemLimitTime(std::string value);
-//    void dataLoad_UserBonusItemLimitTime(std::string value);
-//    void dataLoad_UserMagnetItemLimitTime(std::string value);
-//    void dataLoad_UserMagnetItemLimitRadius(std::string value);
-//    void dataLoad_UserBestTotalScore(std::string value);
-//    void dataLoad_UserBestCombo(std::string value);
-//    void dataLoad_UserCurrentCharacter(std::string value);
-//    void dataLoad_UserCurrentPet(std::string value);
-//    void dataLoad_UserCurrentRocket(std::string value);
-//    void dataLoad_UserCurrentChallenge(std::string value);
-//    void dataLoad_UserCharacterList(std::string value);
-//    void dataLoad_UserPetList(std::string value);
-//    void dataLoad_UserRocketList(std::string value);
-
 
     CUserDataManager();
     virtual ~CUserDataManager();
