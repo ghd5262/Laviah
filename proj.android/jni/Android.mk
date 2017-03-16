49eb91e69bc506a4e53e8606d449a7f5f4d4fbc3
ghd5262 <ghd5262@e3c12c2a-3f9b-4cee-bc1f-3a9786f174a7>
2017-03-16 06:48:18 +0000

    [fix] moved from proj.win32/Classes to Classes
    git-svn-id: svn://www.hshgames.com/project_s@436 e3c12c2a-3f9b-4cee-bc1f-3a9786f174a7

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -12,35 +12,35 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/AI/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/AI/States/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/Common/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/DataManager/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/DataManager/ChallengeRewarder/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/DataManager/ChallengeChecker/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/GameObject/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/GameObject/Bullet/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/GameObject/Shooter/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/json/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/MyUI/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/MyUI/Popup/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/MyUI/Popup/Option/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/Particle/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/Scene/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/Task/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/SDKUtil/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/GoogleCloud/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../proj.win32/Classes/Download/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/AI/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/AI/States/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/Common/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/DataManager/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/DataManager/ChallengeRewarder/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/DataManager/ChallengeChecker/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/GameObject/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/GameObject/Bullet/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/GameObject/Shooter/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/json/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/MyUI/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/MyUI/Popup/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/MyUI/Popup/Option/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/Particle/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/Scene/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/Task/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/SDKUtil/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/GoogleCloud/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/Download/*.cpp)
 
 PROJECT_FILES := $(PROJECT_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
-../../proj.win32/Classes/AppDelegate.cpp$(PROJECT_FILES)
+../../Classes/AppDelegate.cpp$(PROJECT_FILES)
 
 LOCAL_CPPFLAGS := -DSDKBOX_ENABLED
 LOCAL_LDLIBS := -landroid \
 -llog
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../proj.win32/Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 LOCAL_WHOLE_STATIC_LIBRARIES := PluginFacebook \
 sdkbox
 
